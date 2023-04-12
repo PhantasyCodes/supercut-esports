bc315adcbf5c7f4c63b252da2bc53943dae0e4af
David King <davidkingmwai@gmail.com>
2023-04-12 21:06:54 +0300

    Hero is set up

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { motion } from 'framer-motion'
+import gamer from '../../assets/images/gamer.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
 const Hero = () => {
   return (
@@ -6,9 +10,12 @@ const Hero = () => {
         <div className="hero-text">
             <h1>We put our <span>money</span> where our mouth is</h1>
             <p>Fortnite scirms, FIFA boiler rooms, and custom Warzone games</p>
+            <motion.button whileHover={{boxShadow: '1rem 1rem 0 black', transition: {type: 'spring', stiffness: 560, damping: 20,}}} className='button'>Current Tournaments
+                <FontAwesomeIcon style={{marginLeft: '1.5rem'}} icon={faArrowUpRightFromSquare} />
+            </motion.button>
         </div>
         <div className="hero-img">
-
+            <img src={gamer} alt="gamer" />
         </div>
     </div>
   )
