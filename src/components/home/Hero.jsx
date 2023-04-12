5ae3235f5670682747a35ecd54986cdb56273041
David King <davidkingmwai@gmail.com>
2023-04-12 20:15:39 +0300

    More updates

---
+++ src/components/home/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="hero-text">
+            <h1>We put our <span>money</span> where our mouth is</h1>
+            <p>Fortnite scirms, FIFA boiler rooms, and custom Warzone games</p>
+        </div>
+        <div className="hero-img">
+
+        </div>
+    </div>
+  )
+}
+
+export default Hero
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import './Home.css'
+import Hero from '../components/home/Hero'
 
 const Home = () => {
   return (
     <div>
         <Nav />
+        <Hero />
     </div>
   )
 }
