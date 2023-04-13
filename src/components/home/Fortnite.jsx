52324d9af4a7412075f7c48c1b41eb212b68e5be
David King <davidkingmwai@gmail.com>
2023-04-13 11:32:01 +0300

    Updates

--- src/components/home/Fortnite.jsx
+++ src/components/home/Fortnite.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import forniteLogo from '../../assets/images/fortnite-logo.png'
+import fortnite from '../../assets/images/fortnite.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
@@ -7,10 +8,11 @@ const Fortnite = () => {
   return (
     <div className='fortnite-container'>
         <div className="fortnite-text">
-          <img src={forniteLogo} alt="fortnite" />
+          <img className='fortnite-logo' src={forniteLogo} alt="fortnite" />
           <h2>SUPERCUT SCIRMS</h2>
           <p>Custom Fortnite lobbies with cash prizes, and more winnings. Open to all players. Stay tuned on Saturdays and Sundays!</p>
           <button className='button'>active scirms<FontAwesomeIcon style={{marginLeft: '1.5rem'}} icon={faArrowUpRightFromSquare} /></button>
+          <img className='unique' src={fortnite} alt="fortnite" />
         </div>
     </div>
   )
