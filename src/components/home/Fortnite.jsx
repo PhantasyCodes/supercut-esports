03e9852b2317d5b9351c3b94b1268eec754a14c0
David King <davidkingmwai@gmail.com>
2023-04-13 11:46:20 +0300

    Progress

--- src/components/home/Fortnite.jsx
+++ src/components/home/Fortnite.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import forniteLogo from '../../assets/images/fortnite-logo.png'
 import fortnite from '../../assets/images/fortnite.png'
+import { motion } from 'framer-motion'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
 const Fortnite = () => {
   return (
     <div className='fortnite-container'>
-        <div className="fortnite-text">
-          <img className='fortnite-logo' src={forniteLogo} alt="fortnite" />
-          <h2>SUPERCUT SCIRMS</h2>
-          <p>Custom Fortnite lobbies with cash prizes, and more winnings. Open to all players. Stay tuned on Saturdays and Sundays!</p>
-          <button className='button'>active scirms<FontAwesomeIcon style={{marginLeft: '1.5rem'}} icon={faArrowUpRightFromSquare} /></button>
-          <img className='unique' src={fortnite} alt="fortnite" />
-        </div>
+      <img className='unique' src={fortnite} alt="fortnite" />
+      <div className="fortnite-text">
+        <img className='fortnite-logo' src={forniteLogo} alt="fortnite" />
+        <h2><span>SUPERCUT</span> SCIRMS</h2>
+        <p>Custom Fortnite lobbies with cash prizes, and more winnings. Open to all players. Stay tuned on Saturdays and Sundays!</p>
+        <motion.button whileHover={{boxShadow: '1rem 1rem 0 black', transition: {type: 'spring', stiffness: 560, damping: 20,}}} className='button'>active scirms<FontAwesomeIcon style={{marginLeft: '1.5rem'}} icon={faArrowUpRightFromSquare} /></motion.button>
+      </div>
     </div>
   )
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -18,7 +18,6 @@ const Home = () => {
           <Fortnite />
         </ParallaxLayer>
       </Parallax>
-
     </div>
   )
 }
