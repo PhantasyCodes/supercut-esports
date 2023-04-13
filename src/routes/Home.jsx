0c299339a9bf97bbac95e6c1660360032a75f3ab
David King <davidkingmwai@gmail.com>
2023-04-13 12:03:45 +0300

    Update

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,6 +3,7 @@ import Nav from '../components/Nav'
 import './Home.css'
 import Hero from '../components/home/Hero'
 import Fortnite from '../components/home/Fortnite'
+import COD from '../components/home/COD'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 
@@ -16,6 +17,9 @@ const Home = () => {
         </ParallaxLayer>
         <ParallaxLayer offset={0.99} speed={0.4}>
           <Fortnite />
+        </ParallaxLayer>
+        <ParallaxLayer offset={1.4} speed={0.2}>
+          <COD />
         </ParallaxLayer>
       </Parallax>
     </div>
